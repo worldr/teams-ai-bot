f53c265ba2cc5d8d04973b21cceb7daaf8cc6b37
Michael <michael@valarian.com>
2024-10-11 08:36:52 +0000

    Updated MS AI Bot to Fri Oct 11 08:36:40 UTC 2024

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # AI Bot for Microsoft Teams
 
-Thu Oct 10 08:36:54 UTC 2024
+Fri Oct 11 08:36:40 UTC 2024
--- src/app-dev.ts
+++ src/app-dev.ts
@@ -19,7 +19,7 @@ const app = new Application({
 
 app.message("/reset", async (context, state) => {
   state.deleteConversationState();
-  await context.sendActivity("Ok lets start this over Thu Oct 10 08:36:54 UTC 2024.");
+  await context.sendActivity("Ok lets start this over Fri Oct 11 08:36:40 UTC 2024.");
 });
 
 app.ai.action(AI.HttpErrorActionName, async (context, state, data) => {
