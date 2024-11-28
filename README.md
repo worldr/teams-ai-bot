7fa062325a5db76602812995d848bb9141268b76
Michael <michael@valarian.com>
2024-11-28 08:38:29 +0000

    Updated MS AI Bot to Thu Nov 28 08:38:16 UTC 2024

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # AI Bot for Microsoft Teams
 
-Wed Nov 27 08:38:44 UTC 2024
+Thu Nov 28 08:38:16 UTC 2024
--- src/creator-dev.ts
+++ src/creator-dev.ts
@@ -7,7 +7,7 @@ if (!openAIKey) {
 (async () => {
   const assistant = await preview.AssistantsPlanner.createAssistant(openAIKey, {
     name: "Bot Assistant",
-    instructions: "Wed Nov 27 08:38:44 UTC 2024",
+    instructions: "Thu Nov 28 08:38:16 UTC 2024",
     tools: [{ type: "code_interpreter" }],
     model: "gpt-3.5-turbo",
   });
